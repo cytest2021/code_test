cdad20556b9beb1c8bd62b76d7008d3321710929
cy <1605858595@qq.com>
2021-06-06 21:15:40 +0800

    第一次提交

---
+++ hello.java
@@ -0,0 +1,10 @@
+/**
+ * @Project: code_test
+ * @Create: 2021-06-06 21:12
+ * @Desc：
+ **/
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
